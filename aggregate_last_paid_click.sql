8f723e5c81280da8d96c9071dc7c35b53f2f3210
ArmanAbuov <arman.abuov@gmail.com>
2026-05-06 21:13:59 +0500

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,78 +1,81 @@
---модель  Last Paid или click по последней дате
-WITH last_click AS (
-    SELECT DISTINCT ON (visitor_id) *
-    FROM sessions
-    WHERE medium <> 'organic'
-    ORDER BY visitor_id ASC, visit_date DESC
+with visitors_with_leads as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        lower(s.source) as utm_source,
+        row_number() over (
+            partition by s.visitor_id
+            order by s.visit_date desc
+        ) as rn
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
+    where s.medium != 'organic'
 ),
 
--- 2 запрос групировка по дате и join c leads
-group_by_date AS (
-    SELECT
-        la.source AS utm_source,
-        la.medium AS utm_medium,
-        la.campaign AS utm_campaign,
-        la.content AS utm_content,
-        date(la.visit_date) AS visit_date,
-        count(la.visitor_id) AS visitors_count,
-        count(l.lead_id) AS leads_count,
-        count(l.lead_id) FILTER (WHERE l.closing_reason = 'Успешная продажа')
-            AS purchases_count,
-        sum(l.amount) AS revenue
-    FROM last_click AS la
-    LEFT JOIN leads AS l ON la.visitor_id = l.visitor_id
-    GROUP BY date(la.visit_date), la.source, la.medium, la.campaign, la.content
-    ORDER BY revenue DESC NULLS LAST, date(la.visit_date) ASC
-),
-
--- 3 запрос (не связан с двумя выше) сложил vk и ya и сгруппировал по 4 полям
-union_vk_ya AS (
-    SELECT
+aggregated_data as (
+    select
         utm_source,
         utm_medium,
         utm_campaign,
-        utm_content,
-        sum(daily_spent) AS daily_spent
-    FROM (
-        SELECT
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            utm_content,
-            daily_spent
-        FROM vk_ads
-        UNION ALL
-        SELECT
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            utm_content,
-            daily_spent
-        FROM ya_ads
-    )
-    GROUP BY utm_source, utm_medium, utm_campaign, utm_content
+        date(visit_date) as visit_date,
+        count(visitor_id) as visitors_count,
+        count(
+            case
+                when created_at is not null then visitor_id
+            end
+        ) as leads_count,
+        count(case when status_id = 142 then visitor_id end) as purchases_count,
+        sum(case when status_id = 142 then amount end) as revenue
+    from visitors_with_leads
+    where rn = 1
+    group by 1, 2, 3, 4
+),
+
+marketing_data as (
+    select
+        date(campaign_date) as visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by 1, 2, 3, 4
+    union all
+    select
+        date(campaign_date) as visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from vk_ads
+    group by 1, 2, 3, 4
 )
 
--- 4 запрос (join c union (vk+ya))
-SELECT
-    gr.visit_date,
-    gr.utm_source,
-    gr.utm_medium,
-    gr.utm_campaign,
-    sum(gr.visitors_count) AS visitors_count,
-    sum(un.daily_spent) AS total_cost,
-    sum(gr.leads_count) AS leads_count,
-    sum(gr.purchases_count) AS purchases_count,
-    sum(gr.revenue) AS revenue
-FROM group_by_date AS gr
-LEFT JOIN union_vk_ya AS un
-    ON
-        gr.utm_source = un.utm_source
-        AND gr.utm_medium = un.utm_medium
-        AND gr.utm_campaign = un.utm_campaign
-        AND gr.utm_content = un.utm_content
-GROUP BY
-    gr.visit_date, gr.utm_source, gr.utm_medium, gr.utm_campaign
-ORDER BY
-    revenue DESC NULLS LAST, gr.visit_date ASC, visitors_count DESC,
-    gr.utm_source ASC, gr.utm_medium ASC, gr.utm_campaign ASC;
+select
+    a.visit_date,
+    a.visitors_count,
+    a.utm_source,
+    a.utm_medium,
+    a.utm_campaign,
+    m.total_cost,
+    a.leads_count,
+    a.purchases_count,
+    a.revenue
+from aggregated_data as a
+left join marketing_data as m
+    on
+        a.visit_date = m.visit_date
+        and lower(a.utm_source) = m.utm_source
+        and lower(a.utm_medium) = m.utm_medium
+        and lower(a.utm_campaign) = m.utm_campaign
+order by 9 desc nulls last, 1, 2 desc, 3, 4;
